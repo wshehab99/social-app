187d38006f90dd1e0a5f74ea3d74354296887e84
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-08-22 14:03:14 +0200

    App Cubit

--- lib/layout/screens/register_screen.dart
+++ lib/layout/screens/register_screen.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/cubit/app_cubit.dart';
+import 'package:social_media_app/cubit/app_states.dart';
 import 'package:social_media_app/layout/widgets/social_app_button.dart';
 import 'package:social_media_app/layout/widgets/social_app_text_field.dart';
 
@@ -23,137 +25,149 @@ class RegisterScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "Register",
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "Register to our platform to browse hot offers",
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    SocialAppTextField(
-                      type: TextInputType.name,
-                      label: 'name',
-                      prefix: const Icon(Icons.person),
-                      controller: nameController,
-                      validate: (value) {
-                        if (value!.isEmpty) {
-                          return 'please enter your name';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SocialAppTextField(
-                      type: TextInputType.emailAddress,
-                      label: 'E-mail',
-                      prefix: const Icon(Icons.email_outlined),
-                      controller: emailController,
-                      validate: (value) {
-                        if (value!.isEmpty) {
-                          return 'please enter your e-mail address';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SocialAppTextField(
-                      type: TextInputType.phone,
-                      label: 'Phone Number',
-                      prefix: const Icon(Icons.phone),
-                      controller: phoneController,
-                      validate: (value) {
-                        if (value!.isEmpty) {
-                          return 'please enter your phone number';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SocialAppTextField(
-                      type: TextInputType.emailAddress,
-                      label: 'Password',
-                      prefix: const Icon(Icons.lock),
-                      controller: passwordController,
-                      suffix: IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.visibility)),
-                      validate: (value) {
-                        if (value!.isEmpty || value.length < 8) {
-                          return 'Password is too short';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SocialAppTextField(
-                      type: TextInputType.emailAddress,
-                      label: 'Confirm Password',
-                      prefix: const Icon(Icons.lock),
-                      controller: cnofPasswordController,
-                      suffix: IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.visibility)),
-                      validate: (value) {
-                        if (value!.isEmpty || value.length < 8) {
-                          return 'Password is too short';
-                        } else if (cnofPasswordController.text !=
-                            passwordController.text) {
-                          return 'Password not valid';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    SocialAppButton(
-                        text: "REGISTER",
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            register(
-                              emailController.text,
-                              passwordController.text,
-                            );
-                          }
-                        }),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                  ]),
+          child: BlocProvider(
+            create: ((context) => AppCubit()),
+            child: BlocConsumer<AppCubit, AppStates>(
+              listener: ((context, state) {}),
+              builder: ((context, state) {
+                AppCubit cubit = context.read<AppCubit>();
+                return SingleChildScrollView(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Register",
+                            style: Theme.of(context).textTheme.headline2,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Register to our platform to chat with friends",
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          SocialAppTextField(
+                            type: TextInputType.name,
+                            label: 'name',
+                            prefix: const Icon(Icons.person),
+                            controller: nameController,
+                            validate: (value) {
+                              if (value!.isEmpty) {
+                                return 'please enter your name';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SocialAppTextField(
+                            type: TextInputType.emailAddress,
+                            label: 'E-mail',
+                            prefix: const Icon(Icons.email_outlined),
+                            controller: emailController,
+                            validate: (value) {
+                              if (value!.isEmpty) {
+                                return 'please enter your e-mail address';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SocialAppTextField(
+                            type: TextInputType.phone,
+                            label: 'Phone Number',
+                            prefix: const Icon(Icons.phone),
+                            controller: phoneController,
+                            validate: (value) {
+                              if (value!.isEmpty) {
+                                return 'please enter your phone number';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SocialAppTextField(
+                            type: TextInputType.emailAddress,
+                            label: 'Password',
+                            prefix: const Icon(Icons.lock),
+                            controller: passwordController,
+                            isPassword: !cubit.passwordVisabilty,
+                            suffix: IconButton(
+                                onPressed: () {
+                                  cubit.changePasswordVisablity();
+                                },
+                                icon: cubit.passwordVisabilty
+                                    ? const Icon(Icons.visibility)
+                                    : const Icon(Icons.visibility_off)),
+                            validate: (value) {
+                              if (value!.isEmpty || value.length < 8) {
+                                return 'Password is too short';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SocialAppTextField(
+                            type: TextInputType.emailAddress,
+                            label: 'Confirm Password',
+                            prefix: const Icon(Icons.lock),
+                            controller: cnofPasswordController,
+                            isPassword: !cubit.passwordVisabilty,
+                            suffix: IconButton(
+                                onPressed: () {
+                                  cubit.changePasswordVisablity();
+                                },
+                                icon: cubit.passwordVisabilty
+                                    ? const Icon(Icons.visibility)
+                                    : const Icon(Icons.visibility_off)),
+                            validate: (value) {
+                              if (value!.isEmpty || value.length < 8) {
+                                return 'Password is too short';
+                              } else if (cnofPasswordController.text !=
+                                  passwordController.text) {
+                                return 'Password not valid';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          SocialAppButton(
+                              text: "REGISTER",
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  cubit.register(
+                                    emailController.text,
+                                    passwordController.text,
+                                  );
+                                }
+                              }),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                        ]),
+                  ),
+                );
+              }),
             ),
           ),
         ),
       ),
     );
-  }
-
-  Future<void> register(String email, String password) async {
-    await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: password)
-        .then((value) {
-      print(value.user!.uid);
-      print(value.user!.email);
-    });
   }
 }
